ba6f2a3647758cbb9a7c4f9c7010c78c75c494a9
RobertWeide <weide@kyos.com>
2017-02-09 15:34:53 +0100

    Restart 1 hour after everuthing is done

--- kyos-crawler.sh
+++ kyos-crawler.sh
@@ -1,6 +1,11 @@
 if [ -f kyos-crawler-running.txt ]; then
     exit
 fi
+if [ `expr $(date +%s) - $(date +%s -r checked.lst)` -ge 3600 ]; then
+    foldername = `date +"%Y%m%d%H%M"`
+    mkdir $foldername
+    mv cookies.txt links.lst checked.lst $foldername
+fi
 echo 'Running' > kyos-crawler-running.txt
 casperjs --cookies-file=cookies.txt kyos-crawler.js http://win.kyos.com/develop/
 rm kyos-crawler-running.txt
