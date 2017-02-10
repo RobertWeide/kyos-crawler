7ccb8abe3f17a32e9a538733e206436b8c4c09c3
KYOS client test server <server@kyos.com>
2017-02-10 14:26:21 +0100

    Move crawler log when starting new run.

--- kyos-crawler.sh
+++ kyos-crawler.sh
@@ -5,7 +5,7 @@ fi
 if [ `expr $(date +%s) - $(date +%s -r checked.lst)` -ge 3600 ]; then
     foldername = `date +"%Y%m%d%H%M"`
     mkdir $foldername
-    mv cookies.txt links.lst checked.lst $foldername
+    mv cookies.txt links.lst checked.lst crawler.log $foldername
 fi
 echo 'Running' > kyos-crawler-running.txt
 casperjs --cookies-file=cookies.txt kyos-crawler.js http://win.kyos.com/develop/
