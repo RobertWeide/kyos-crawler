64fbeffcc1ad8736bfffd3baf8fc906ac6cc57fa
KYOS client test server <server@kyos.com>
2017-03-20 14:23:36 +0100

    Output to log from script instead of crontab.

--- kyos-crawler.sh
+++ kyos-crawler.sh
@@ -8,5 +8,5 @@ if [ `expr $(date +%s) - $(date +%s -r checked.lst)` -ge 3600 ]; then
     mv cookies.txt links.lst checked.lst crawler.log $foldername
 fi
 echo 'Running' > kyos-crawler-running.txt
-casperjs --cookies-file=cookies.txt kyos-crawler.js http://win.kyos.com/develop/
+casperjs --cookies-file=cookies.txt kyos-crawler.js http://win.kyos.com/develop/ >> /kyos/kyos-crawler/crawler.log
 rm kyos-crawler-running.txt
