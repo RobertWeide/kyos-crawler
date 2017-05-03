c98f7ec46bfd670de1a16171b8cb8c05a386af9a
KYOS client test server <server@kyos.com>
2017-05-03 10:07:05 +0200

    New windev server + https

--- kyos-crawler.sh
+++ kyos-crawler.sh
@@ -8,5 +8,5 @@ if [ `expr $(date +%s) - $(date +%s -r checked.lst)` -ge 3600 ]; then
     mv cookies.txt links.lst checked.lst crawler.log $foldername
 fi
 echo 'Running' > kyos-crawler-running.txt
-casperjs --cookies-file=cookies.txt kyos-crawler.js http://win.kyos.com/develop/ >> /kyos/kyos-crawler/crawler.log
+casperjs --cookies-file=cookies.txt --ssl-protocol=any --ignore-ssl-errors=true kyos-crawler.js https://windev.kyos.com/ >> /kyos/kyos-crawler/crawler.log
 rm kyos-crawler-running.txt
